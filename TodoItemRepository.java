3f0a43b402ebc3ee871108f910c0699d902a3642
shreevazz <131843726+shreevazz@users.noreply.github.com>
2023-11-24 15:50:58 +0530

    Delete TodoItemRepository.java

--- TodoItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.erikyuzwa.springboottodo.repositories;
-
-import com.erikyuzwa.springboottodo.models.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long>  {
-    
-}
